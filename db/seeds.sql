d0445ead312709e266a81f44bf3244f3b48dac15
Allison <andersonaallison@gmail.com>
2019-06-23 13:11:50 -0600

    setting up basic skeleton

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+insert into burgers (burger_name, devoured)
+values ("Turkey Cheese Burger", false);
+
+insert into burgers (burger_name, devoured)
+values ("Impossible Burger", false);
+
+insert into burgers (burger_name, devoured)
+values ("Bean Burger", false);
