473afd3af84689d80ad3596f75c30241390bd3b6
Allison <andersonaallison@gmail.com>
2019-06-23 22:59:51 -0600

    changed back to non Heroku name

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS `fzuqoq63fu1d5ek7`;
-CREATE DATABASE `fzuqoq63fu1d5ek7`;
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
 
-USE `fzuqoq63fu1d5ek7`;
+USE burger_db;
 
 create table burgers (
   id int(1) NOT NULL AUTO_INCREMENT,
